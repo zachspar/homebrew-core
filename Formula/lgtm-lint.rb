f50bd93d548fc70a7c9fd3c28f601c377f59eaad
Zachary Spar <zachspar@gmail.com>
2023-05-19 20:10:23 -0400

    update homepage

--- Formula/lgtm-lint.rb
+++ Formula/lgtm-lint.rb
@@ -2,7 +2,7 @@ class LgtmLint < Formula
   include Language::Python::Virtualenv
 
   desc "Fully featured 'Looks good to me!' linter"
-  homepage "https://github.com/maxwellpaulm/lgtm-lint"
+  homepage "https://pypi.org/project/lgtm-lint/"
   url "https://files.pythonhosted.org/packages/ed/75/8fabbc813382ed319f86ef176154df2bcaf66c9e5c1c9353cacddc480753/lgtm-lint-1.0.2.tar.gz"
   sha256 "90ce1c8b719e276d05218d2737f77248fe336708297b7c91ebdc87630d5104f5"
   license "Unlicense"
